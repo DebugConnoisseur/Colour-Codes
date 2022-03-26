00103fc05fb530c3caf3d91013bea356c996d155
cssDuke <88923986+cssDuke@users.noreply.github.com>
2022-03-26 12:11:56 +0530

    Create colour_codes.cs

---
+++ main/colour_codes.cs
@@ -0,0 +1,102 @@
+namespace Colour_Codes
+{
+    class colourcode
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---------------------------Colour Codes------------------------------------");
+
+            Console.Write("Enter the name of the colour in lowercase:  ");
+            string colour = Console.ReadLine();
+
+            if (colour == "red")
+            {
+                Console.WriteLine("Colour code for Red is #FF0000");
+            }
+            else if (colour == "cyan")
+            {
+                Console.WriteLine("Colour Code for Cyan is #00FFFF");
+            }
+            else if (colour == "blue")
+            {
+                Console.WriteLine("Colour Code for Blue is #0000FF");
+            }
+            else if (colour == "darkblue")
+            {
+                Console.WriteLine("Colour Code for DarkBlue is #00008B");
+            }
+            else if (colour == "lightblue")
+            {
+                Console.WriteLine("Colour Code for LightBlue is #ADD8E6");
+            }
+            else if (colour == "purple")
+            {
+                Console.WriteLine("Colour Code for Purple is #800080");
+            }
+            else if (colour == "yellow")
+            {
+                Console.WriteLine("Colour Code for Yellow is #FFFF00");
+            }
+            else if (colour == "lime")
+            {
+                Console.WriteLine("Colour Code for Lime is #00FF00");
+            }
+            else if (colour == "Magneta")
+            {
+                Console.WriteLine("Colour Code for Magneta is #FF00FF");
+            }
+            else if (colour == "pink")
+            {
+                Console.WriteLine("Colour Code for Pink is #FFC0CB");
+            }
+            else if (colour == "white")
+            {
+                Console.WriteLine("Colour Code for White is #FFFFFF");
+            }
+            else if (colour == "silver")
+            {
+                Console.WriteLine("Colour Code for Silver is #C0C0C0");
+            }
+            else if (colour == "grey" || colour == "gray")
+            {
+                Console.WriteLine("Colour Code for Grey or Gray is #808080");
+            }
+            else if (colour == "black")
+            {
+                Console.WriteLine("Colour Code for Black is #000000");
+            }
+            else if (colour == "orange")
+            {
+                Console.WriteLine("Colour Code for Orange is #FFA500");
+            }
+            else if (colour == "brown")
+            {
+                Console.WriteLine("Colour Code for Brown is #A52A2A");
+            }
+            else if (colour == "maroon") 
+            {
+                Console.WriteLine("Colour Code for Maroon is #800000");
+            }
+            else if (colour == "green")
+            {
+                Console.WriteLine("Colour Code for Green is #008000");
+            }
+            else if (colour == "olive")
+            {
+                Console.WriteLine("Colour Code for Olive is #808000");
+            }
+            else if (colour == "aquamarine")
+            {
+                Console.WriteLine("Colour Code for Aquamarine is #7FFD4");
+            }
+
+            Console.ReadLine();
+        }
+        
+
+            
+        
+        
+    }
+}
